f732a05a2da8e184addb20073a8aa399c8d6b268
GrO0vy <lmc2819@naver.com>
2025-08-15 22:06:12 +0900

    메서드 순서 변경

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,13 +7,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(DemoApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
